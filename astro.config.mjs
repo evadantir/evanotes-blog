cd402fc7b6738a8c199952e2f6320612f9b84116
evadantir <11324759+evadantir@users.noreply.github.com>
2026-08-17 18:37:11 +0900

    ➖ del: unused llib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 import emdash, { local } from "emdash/astro";
 import { sqlite } from "emdash/db";
