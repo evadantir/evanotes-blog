449cff481aa16ba5ebbf0bc4166d5a9cf44188dc
evadantir <11324759+evadantir@users.noreply.github.com>
2026-08-22 15:34:30 +0900

    💄 ui: change label name and the font

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,18 +22,24 @@ export default defineConfig({
 		fonts: [
 		{
 			provider: fontProviders.google(),
-			name: "Inter",
+			name: "Plus Jakarta Sans",
 			cssVariable: "--font-body",
 			weights: [400, 500, 600, 700],
 			fallbacks: ["sans-serif"],
 		},
 		{
-			provider: fontProviders.google(),
-			name: "JetBrains Mono",
+			provider: fontProviders.fontsource(),
+			name: "UnifontEX",
 			cssVariable: "--font-mono",
-			weights: [400, 500],
+			weights: [400],
 			fallbacks: ["monospace"],
 		},
+		{
+			provider: fontProviders.google(),
+			name: "Lobster Two",
+			cssVariable: "--font-calligraphy",
+			weights: [400, 700]
+		}
 	],
 	devToolbar: { enabled: false },
 });
